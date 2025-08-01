4678fddf68ab8bc8f0f4080020adc0e70be584e9
Slimpleton <daniel.gione@gmail.com>
2025-08-01 09:36:56 -0700

    moved todos to a blank component instead i think it fits there better? if i move the search functionality out later sobeit

--- src/app/plant-search/plant-search.component.ts
+++ src/app/plant-search/plant-search.component.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Component } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
+@Component({
+  selector: 'app-plant-search',
+  imports: [],
+  templateUrl: './plant-search.component.html',
+  styleUrl: './plant-search.component.css'
 })
-export class PlantSearchService {
+export class PlantSearchComponent {
   // TODO sorting system, want to be able to sort by common name / scientific name asc / desc
   // TODO swap between user location and specific location // county and state
   // TODO figure out use case when the plant is native to state but has no county data? do i just include all or none for now
   // TODO filtering system, make maybe the thing and above both into the search service??
-
-
+  // TODO use input output to input unfiltered stuff output filtered stuff and display on the same html?? might work idk
 }
