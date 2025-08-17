0e2e56146e0f0b3c9a22a4ab7c8f4383b05620c6
Slimpleton <daniel.gione@gmail.com>
2025-08-17 09:02:24 -0700

    got the canvas to render within a 975 x 610 viewport !!

--- src/app/county-map/county-map.component.ts
+++ src/app/county-map/county-map.component.ts
@@ -1,7 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as d3 from 'd3-geo';
 import * as topojson from 'topojson-client';
-import * as us from 'us-atlas/counties-10m.json';
+import * as us from 'us-atlas/counties-albers-10m.json';
 
 @Component({
   selector: 'app-county-map',
@@ -9,25 +9,24 @@ import * as us from 'us-atlas/counties-10m.json';
   templateUrl: './county-map.component.html',
   styleUrl: './county-map.component.css'
 })
-export class CountyMapComponent {
+export class CountyMapComponent implements AfterViewInit {
 
   @ViewChild('mapCanvas') public canvas!: ElementRef<HTMLCanvasElement>;
-  private usa : any = us;
+  private usa: any = us;
 
-  public constructor() { }
+  public constructor() {
 
-  public fcn(): void {
-    const width = 975;
-    const height = 610;
-    // TODO replace with canvas call wit angular
-    const context = DOM.context2d(width, height);
+  }
+  ngAfterViewInit(): void {
+    const context = this.canvas.nativeElement.getContext('2d');
+    if (!context)
+      return;
     const path = d3.geoPath(null, context);
-    context.canvas.style.maxWidth = "100%";
     context.lineJoin = "round";
     context.lineCap = "round";
 
     context.beginPath();
-    path(topojson.mesh(this.usa, this.usa.objects.counties, (a, b) => a !== b && (a.id ?? 0 / 1000 | 0) === (b.id ?? 0 / 1000 | 0)));
+    path(topojson.mesh(this.usa, this.usa.objects.counties, (a: any, b: any) => a !== b && (a.id / 1000 | 0) === (b.id  / 1000 | 0)));
     context.lineWidth = 0.5;
     context.strokeStyle = "#aaa";
     context.stroke();
@@ -43,7 +42,6 @@ export class CountyMapComponent {
     context.lineWidth = 1;
     context.strokeStyle = "#000";
     context.stroke();
-
-    return context.canvas;
   }
+
 }
