17c0b96cb21a041c16c464b2f763117da45dce3b
Slimpleton <daniel.gione@gmail.com>
2025-08-30 10:09:56 -0700

    Added a stylized icon for image search, made it clickable and open a google image search of the item's common name / scientific name

---
+++ src/app/pic-search-icon/pic-search-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'pic-search-icon',
+  imports: [MatIconModule],
+  templateUrl: './pic-search-icon.component.html',
+  styleUrl: './pic-search-icon.component.css'
+})
+export class PicSearchIconComponent {
+
+}
--- src/app/plant-tile/plant-tile.component.ts
+++ src/app/plant-tile/plant-tile.component.ts
@@ -5,17 +5,24 @@ import { ShadyIconComponent } from '../shady-icon/shady-icon.component';
 import { TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { GovPlantsDataService } from '../services/PLANTS_data.service';
 import { TranslocoModule } from '@jsverse/transloco';
+import { PicSearchIconComponent } from "../pic-search-icon/pic-search-icon.component";
 
 @Component({
   selector: 'plant-tile',
-  imports: [MatIconModule, ShadyIconComponent, TitleCasePipe, UpperCasePipe, TranslocoModule],
+  imports: [MatIconModule, ShadyIconComponent, TitleCasePipe, UpperCasePipe, TranslocoModule, PicSearchIconComponent],
   templateUrl: './plant-tile.component.html',
   styleUrl: './plant-tile.component.css'
 })
 export class PlantTileComponent {
   public usdaGovPlantProfileUrl: string = GovPlantsDataService.usdaGovPlantProfileUrl;
-  @Input({required: true}) public plant!: PlantData;
+  @Input({ required: true }) public plant!: PlantData;
 
-  public constructor(){
+  public constructor() {
+  }
+
+  public openImageSearch(plant: PlantData) {
+    const query : string = plant.commonName?.length > 0 ? plant.commonName : plant.scientificName;
+    const queryUrl : string = `https://www.google.com/search?q=${query}&tbm=isch`;
+    window.open(queryUrl, '_blank');
   }
 }
