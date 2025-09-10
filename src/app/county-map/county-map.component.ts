9b68a0fb31401d9867ac9240cccb3c890474864d
Slimpleton <daniel.gione@gmail.com>
2025-09-10 12:45:12 -0700

    created a drop down that lists all counties with the state abbrev next to it

--- src/app/county-map/county-map.component.ts
+++ src/app/county-map/county-map.component.ts
@@ -3,22 +3,29 @@ import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
 import * as us from 'us-atlas/counties-albers-10m.json';
 import { PositionService } from '../services/position.service';
-import { Subject, takeUntil } from 'rxjs';
+import { filter, map, Subject, takeUntil, tap } from 'rxjs';
 import { combineCountyFIP, County } from '../models/gov/models';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FileService } from '../services/file.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-county-map',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, AsyncPipe],
   templateUrl: './county-map.component.html',
   styleUrl: './county-map.component.css'
 })
 export class CountyMapComponent implements AfterViewInit {
-  private readonly _usa: any = us;
   private readonly _destroy$: Subject<void> = new Subject<void>();
+
+  private readonly _usa: any = us;
+  public readonly counties = this.fileService.counties$.pipe(
+    map((counties) => counties.sort((a,b) => a.countyName.localeCompare(b.countyName))),
+    takeUntil(this._destroy$));
+  public countyName: string | undefined | null = undefined;
   @ViewChild('mapCanvas') private readonly _canvas!: ElementRef<HTMLCanvasElement>;
 
-  public constructor(private readonly _positionService: PositionService){
+  public constructor(private readonly _positionService: PositionService, public readonly fileService: FileService){
   }
 
   public ngAfterViewInit(): void {
@@ -29,18 +36,15 @@ export class CountyMapComponent implements AfterViewInit {
     this._canvas.nativeElement.addEventListener('click', (ev: MouseEvent) => {
       const x : number = ev.offsetX;
       const y : number = ev.offsetY;
-
-      console.log(x,y, );      
+      console.log(x,y);      
       // TODO use the inverse of the transformation to display canvas to get lat/long
       // use that to find what county the click is in
     });
 
-    
     const path = d3.geoPath(null, context);
     context.lineJoin = "round";
     context.lineCap = "round";
 
-  
     context.beginPath();
     path(topojson.mesh(this._usa, this._usa.objects.counties, (a: any, b: any) => a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0)));
     context.lineWidth = 0.5;
@@ -59,7 +63,16 @@ export class CountyMapComponent implements AfterViewInit {
     context.strokeStyle = "#000";
     context.stroke();
 
-    this._positionService.countyEmitter$.pipe(takeUntil(this._destroy$)).subscribe({
+    this._positionService.countyEmitter$.pipe(
+      filter((x) => x != null),
+      map((county) => combineCountyFIP(county)),
+      this.fileService.getCountyCSVItemAsync(),
+      tap((county) => this.countyName = county?.countyName),
+      takeUntil(this._destroy$)
+    ).subscribe();
+
+    this._positionService.countyEmitter$.pipe(
+      takeUntil(this._destroy$)).subscribe({
       next: (county: County) => {
         const fullFip: string = combineCountyFIP(county);
         context.beginPath();
