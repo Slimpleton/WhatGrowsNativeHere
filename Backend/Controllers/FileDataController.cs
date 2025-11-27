dce0b6256eb6d2e42feb2e620ee181ecdca77b36
Slimpleton <daniel.gione@gmail.com>
2025-11-26 20:59:35 -0800

    simplifying controllers

--- Backend/Controllers/FileDataController.cs
+++ Backend/Controllers/FileDataController.cs
@@ -12,43 +12,19 @@ namespace Backend.Controllers
         [HttpGet("plantdata")]
         public async Task<ActionResult<IEnumerable<PlantData>>> GetPlantDataAsync()
         {
-            try
-            {
-                return Ok(FileService.PlantData);
-            }
-            catch (Exception e)
-            {
-                string s = string.Empty;
-                throw;
-            }
+            return Ok(FileService.PlantData);
         }
 
         [HttpGet("states")]
         public async Task<ActionResult<IEnumerable<StateCSVItem>>> GetStatesAsync()
         {
-            try
-            {
-                return Ok(FileService.States);
-            }
-            catch (Exception e)
-            {
-                string s = string.Empty;
-                throw;
-            }
+            return Ok(FileService.States);
         }
 
         [HttpGet("counties")]
         public async Task<ActionResult<IEnumerable<CountyCSVItem>>> GetCountiesAsync()
         {
-            try
-            {
-                return Ok(FileService.Counties);
-            }
-            catch (Exception e)
-            {
-                string s = string.Empty;
-                throw;
-            }
+            return Ok(FileService.Counties);
         }
     }
 }
--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -32,6 +32,6 @@ export const routes: Routes = [
         component: PlantOverviewComponent,
         resolve: {
             plant: plantOverviewResolver
-        }
+        },
     }
 ];
